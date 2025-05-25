d6e5578c3e72aa292d2e4cae2a153ecbe94df48e
utsurea <utsurea@gmail.com>
2025-05-25 23:13:41 +0900

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DTCGGame from './DTCGGame';
+
+function App() {
+  return (
+    <div className="App">
+      <DTCGGame />
+    </div>
+  );
+}
+
+export default App;
